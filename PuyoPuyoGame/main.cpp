10653febab2c4c48df0fb95046a524632b8c8913
Carlos Tirado <darth_carlos_90@hotmail.com>
2016-02-15 19:18:46 -0600

    First commit.

---
+++ PuyoPuyoGame/main.cpp
@@ -0,0 +1,13 @@
+// Main class, here is where the main method is and where the game starts.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Welcome to my Puyo Puyo Game" << endl;
+
+	int exit;
+	cin >> exit;
+	return 0;
+}
